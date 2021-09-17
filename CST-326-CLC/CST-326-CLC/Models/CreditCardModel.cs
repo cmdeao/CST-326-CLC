f64e3c1f19560923f9847fbfbf80777e57523723
Dylan Dorn <ddorn343@gmail.com>
2021-09-16 21:37:50 -0700

    Added Credit Card Model

---
+++ CST-326-CLC/CST-326-CLC/Models/CreditCardModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CST_326_CLC.Models
+{
+    public class CreditCardModel
+    {
+        public enum CardType
+        {
+            Visa,
+            MasterCard,
+            AmericanExpress,
+            Discover
+        }
+
+        [Required(ErrorMessage = "The Card Holder Name field is required.")]
+        public string cardHolderName { get; set; }
+
+        [Required(ErrorMessage = "The Card Type field is required")]
+        public CardType cardType { get; set; }
+
+        [Required(ErrorMessage = "The Card Number field is required.")]
+        [StringLength(16, ErrorMessage = "Invalid card numbers entered", MinimumLength = 15)]
+        public string cardNumber { get; set; }
+
+        [Required(ErrorMessage = "The CVV Code field is required.")]
+        public int cvvCode { get; set; }
+
+        [Required(ErrorMessage = "The Expiration Month field is required.")]
+        public int expirationMonth { get; set; }
+
+        [Required(ErrorMessage = "The Expiration Year field is required.")]
+        public int expirationYear { get; set; }
+
+    }
+}
