3b32c6cd6b63fa8c4b418094cf5575ce7c90ecac
Tim James <32233010+tjames222@users.noreply.github.com>
2021-09-18 14:09:12 -0700

    Added Payment view and controller with functionality at 70%

---
+++ CST-326-CLC/CST-326-CLC/Controllers/PaymentController.cs
@@ -0,0 +1,56 @@
+﻿using CST_326_CLC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CST_326_CLC.Controllers
+{
+    public class PaymentController : Controller
+    {
+        // GET: Payment
+        public ActionResult Index()
+        {
+            return View("PaymentDetails");
+        }
+    }
+
+    /* Refactor to work with payment form */
+
+    //[HttpPost]
+    //public ActionResult ProcessPayment(ShipmentModel model)
+    //{
+    //    Log.Information("User attempting to create shipment...");
+
+    //    // Can be used to display cost to user
+    //    decimal shipmentCost = model.CalculateCost(model.Zip, model.Length, model.Width,
+    //        model.Height, model.Weight, model.DeliveryOption);
+
+    //    if (!ModelState.IsValid)
+    //    {
+    //        Log.Information("Create Shipment: The ModelState was invalid.");
+    //        return View("ShipmentIndexTEST");
+    //    }
+
+    //    ShipmentService service = new ShipmentService();
+
+    //    if (service.AddShipment(model))
+    //    {
+    //        Log.Information("Create Shipment: Shipment succesfully created ID: {0}", model.ShipmentId);
+    //        return Content(String.Format("Created shipment: {0}", model.ShipmentId));
+    //    }
+    //    else
+    //    {
+    //        Log.Information("Create Shipment: Failed to create the shipment.");
+    //        return Content(String.Format("Failed to create shipment: {0}", model.ShipmentId));
+    //    }
+    //}
+
+    public class PaymentInformation
+    {
+        public ShipmentModel Shipment { get; set; }
+        public AddressModel Billing { get; set; }
+        public CreditCardModel CreditCard { get; set; }
+    }
+}
